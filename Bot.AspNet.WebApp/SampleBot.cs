7149e4434fca32650e8c8bf228bde0aacd9a6689
Drew Marsh <drmarsh@microsoft.com>
2018-12-15 11:19:19 -0800

    Minimal Hello World Bot
     * Adds NuGet ASP.NET Core Integration package
     * Configures bot in `Startup.cs`
     * Adds `SampleBot.cs` with rudimentary "Hello World!" response to everything

---
+++ Bot.AspNet.WebApp/SampleBot.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+
+namespace HackedBrain.BotBuilder.Samples.IdiomaticNetCore.BotWebApp
+{
+    public class SampleBot : IBot
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await turnContext.SendActivityAsync("Hello World!", cancellationToken: cancellationToken);
+        }
+    }
+}
