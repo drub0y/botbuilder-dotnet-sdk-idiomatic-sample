399b47fe16c97620630dca5d2851e8a7e7276e22
Drew Marsh <drmarsh@microsoft.com>
2018-12-16 10:01:03 -0800

    Load endpoint app identity from .bot file

--- Bot.AspNet.WebApp/Startup.cs
+++ Bot.AspNet.WebApp/Startup.cs
@@ -2,28 +2,34 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Configuration;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 
 namespace HackedBrain.BotBuilder.Samples.IdiomaticNetCore.BotWebApp
 {
     public class Startup
     {
-        private readonly IConfiguration _configuration;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment hostingEnvironment, IConfiguration configuration)
         {
-            _configuration = configuration ?? throw new System.ArgumentNullException(nameof(configuration));
+            _hostingEnvironment = hostingEnvironment ?? throw new System.ArgumentNullException(nameof(hostingEnvironment));
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var botConfiguration = BotConfiguration.LoadFromFolder(_hostingEnvironment.ContentRootPath);
+
+            var endpoint = botConfiguration.Services.OfType<EndpointService>().FirstOrDefault(ep => ep.Name == _hostingEnvironment.EnvironmentName);
+
             services.AddBot<SampleBot>(options =>
             {
                 options.CredentialProvider = new SimpleCredentialProvider(
-                    _configuration.GetValue<string>("BotCredentials:AppId"),
-                    _configuration.GetValue<string>("BotCredentials:AppPassword"));
+                    endpoint.AppId,
+                    endpoint.AppPassword);
             });
         }
 
