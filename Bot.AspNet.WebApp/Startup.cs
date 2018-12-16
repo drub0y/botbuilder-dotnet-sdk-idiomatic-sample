be112ee1c80c9205bf5bff77e0032576daad6c01
Drew Marsh <drmarsh@microsoft.com>
2018-12-16 09:36:25 -0800

    Set CredentialProvider using values from appsettings

--- Bot.AspNet.WebApp/Startup.cs
+++ Bot.AspNet.WebApp/Startup.cs
@@ -2,15 +2,29 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HackedBrain.BotBuilder.Samples.IdiomaticNetCore.BotWebApp
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new System.ArgumentNullException(nameof(configuration));
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddBot<SampleBot>();
+            services.AddBot<SampleBot>(options =>
+            {
+                options.CredentialProvider = new SimpleCredentialProvider(
+                    _configuration.GetValue<string>("BotCredentials:AppId"),
+                    _configuration.GetValue<string>("BotCredentials:AppPassword"));
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
